d0d28e1ea568d926ca6ae61285284ab27935d674
BrofessorTec <tcampbell59@yahoo.com>
2023-04-27 17:17:14 -0400

    Project looks complete now. Finalizing this dev branch and will create another for other feastures.

--- Project5/Program.cs
+++ Project5/Program.cs
@@ -18,18 +18,177 @@ namespace Project5
     {
         public static void Main()
         {
-        }
-    }
 
-    public void PlayerCombat(Player player, Monster monster, int Damage)
-    {
-        if (Damage > 0)
-        {
-            Console.WriteLine(!"{player.GetName()} hit {monster.GetName()} for {Damage} damage!");
+            //testing some Driver code here for dungeon generation
+            Player userChar;
+            Dungeon dungeon;
+            Monster cellMonster;
+            bool gameOver = false;
+            string? userDir = "go east";
+
+            userChar = new Player(GetPlayerName());
+            dungeon = new Dungeon();
+
+            Console.WriteLine($"You are very brave, {userChar.GetName()}.\nWelcome... to the cube.");
+            Console.Write("Enter any key to continue.");
+            Console.ReadLine();
+            Console.Clear();
+
+            while (!gameOver)
+            {
+                try
+                {
+                    //probably add the map to the top of the screen?
+                    Console.WriteLine($"The current room is cell {dungeon.GetCurrRoom()}, and you have {userChar.GetHealth()} HP left.\n");  //this will just be for debugging, need to add map later
+
+                    if (dungeon.RoomHasWep())
+                    {
+                        Console.WriteLine($"You found a {dungeon.GetRoomWeapon().GetName()}! Your attack power has increased by {dungeon.GetRoomWeapon().GetDamage()}.\n");
+                        userChar.SetDamage(dungeon.GetRoomWeapon().GetDamage());
+                    }
+
+                    //add monster chance here
+                    if (dungeon.RoomHasMonster())
+                    {
+                        cellMonster = dungeon.GetRoomMonster();
+                        Console.WriteLine($"There is a {cellMonster.GetName()} here!\n");
+                        while (cellMonster.GetHealth() > 0 && userChar.GetHealth() > 0)
+                        {
+                            if (userChar.Attack() != 0)
+                            {
+                                Console.WriteLine($"{userChar.GetName()} hits with an attack for {userChar.GetDamage()} damage!");
+                                cellMonster.SetHealth(userChar.GetDamage());
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{userChar.GetName()} misses with an attack!");
+                            }
+
+                            if (cellMonster.Attack() != 0)
+                            {
+                                Console.WriteLine($"{cellMonster.GetName()} hits {userChar.GetName()} with an attack for {cellMonster.GetDamage()} damage!");
+                                userChar.SetHealth(cellMonster.GetDamage());
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{cellMonster.GetName()} misses {userChar.GetName()} with an attack!");
+                            }
+
+                            if (userChar.GetHealth() <= 0)
+                            {
+                                Console.WriteLine("\nEnter any key to continue.");
+                                Console.ReadLine();
+                                Console.Clear();
+                                Console.WriteLine($"The current room is cell {dungeon.GetCurrRoom()}, and you have 0 HP left.\n");  //this will just be for debugging, need to add map later
+                                Console.WriteLine($"{userChar.GetName()} is dead. The game is over!");
+                                gameOver = true;
+                                //Console.ReadLine();
+                            }
+                            if (cellMonster.GetHealth() > 0)
+                            {
+                                Console.WriteLine("\nEnter any key to continue.");
+                                Console.ReadLine();
+                                Console.Clear();
+                                Console.WriteLine($"The current room is cell {dungeon.GetCurrRoom()}, and you have {userChar.GetHealth()} HP left.\n");  //this will just be for debugging, need to add map later
+                            }
+                        }
+                        if (userChar.GetHealth() > 0)
+                        {
+                            Console.WriteLine($"\n{userChar.GetName()} has defeated the {cellMonster.GetName()}!");
+                            Console.WriteLine("\nEnter any key to continue.");
+                            Console.ReadLine();
+                            Console.Clear();
+                            Console.WriteLine($"The current room is cell {dungeon.GetCurrRoom()}, and you have {userChar.GetHealth()} HP left.\n");  //this will just be for debugging, need to add map later
+                        }
+                    }
+
+                    if (userChar.GetHealth() <= 0)
+                    {
+                        /* //moving this code to be right after the attack now
+                        Console.Clear();
+                        Console.WriteLine($"{userChar.GetName()} is dead. The game is over!");
+                        gameOver = true;
+                        Console.ReadLine();
+                        */
+                    }
+                    else
+                    {
+                        Console.WriteLine("What would you like to do?" +
+                            "\nPlease enter \"Go East\" or \"Go West\"");
+                        userDir = Console.ReadLine();
+                        int dirCheck;
+
+                        if (userDir.ToLower() == "go east")
+                        {
+                            dirCheck = dungeon.GoRight();
+                            if (dirCheck == -1)
+                            {
+                                Console.Clear();
+
+                                Console.WriteLine($"You have beaten the dungeon, {userChar.GetName()}! You win!");
+                                gameOver = true;
+                                break;
+                            }
+                        }
+                        else if (userDir.ToLower() == "go west")
+                        {
+                            dirCheck = dungeon.GoLeft();
+                            if (dirCheck == 0)
+                            {
+                                Console.WriteLine($"\nSorry {userChar.GetName()}, but you can't go in that direction.\nEnter any key to continue.");
+                                Console.ReadLine();
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception();
+                        }
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine($"\nI do not know what you mean, {userChar.GetName()}.\nEnter any key to continue.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                Console.Clear();
+            }
+            Console.ReadLine();  //pausing the console before closing out of the game
         }
-        else
+
+        public static string GetPlayerName()
         {
-            Console.WriteLine(!"{player.GetName()} missed {monster.GetName()}!");
+            string? userName = "Dale";
+            bool userNameValid = false;
+            while (!userNameValid)
+            {
+                try
+                {
+                    Console.Write("Hello and welcome to Zork! \nWhat is your name, adventurer?\nName: ");
+                    userName = Console.ReadLine();
+                    userNameValid = true;
+                    Console.Clear();
+                }
+                catch
+                {
+                    Console.Write("That is not a valid entry.\nEnter any key to continue.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+            return userName;
         }
+
+        /*public void PlayerCombat(Player player, Monster monster, int Damage)  //this might be used for combat later? i think this would only be needed if there was multiple enemies at once and you need to select one
+        {
+            if (Damage > 0)
+            {
+                Console.WriteLine(!"{player.GetName()} hit {monster.GetName()} for {Damage} damage!");
+            }
+            else
+            {
+                Console.WriteLine(!"{player.GetName()} missed {monster.GetName()}!");
+            }
+        }*/
     }
 }
